015dcc4ef4bba044048e0b9d4f862fefee19336f
edwinl115 <144479314+edwinl115@users.noreply.github.com>
2023-10-03 13:15:42 -0400

    Add files via upload

---
+++ LinProj1Part2.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+public class LinProj1Part2{
+    public static void main (String[]args){
+       
+       String operation = JOptionPane.showInputDialog("Please enter a statement like the example \n Example: +,3,4,2,1");
+       String substring = operation.substring(2);
+       String input = operation.substring(0,1);
+       Integer.parseInt(substring);
+       //char one = operation.charAt(1);
+       //char integer = operation.charAt(3);
+       
+       if(input.equals("+")){
+           
+       }
+           
+       }
+    }
+
