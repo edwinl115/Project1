72214792ebfd001b1feead19d554ad861618ec60
edwinl115 <144479314+edwinl115@users.noreply.github.com>
2023-10-03 13:15:27 -0400

    Add files via upload

---
+++ LinProj1Part1.java
@@ -0,0 +1,45 @@
+public class LinProj1Part1{
+    public static void main (String[]args){
+        
+        //3)
+        int a = 15;
+        int b = 2;
+        int sum = a+b;
+        System.out.println(sum);
+        
+        //4)
+        int c = 51;
+        double d = 21.32;
+        double difference = c-d;
+        System.out.println(difference);
+        
+        //5)
+        double e = 5.0;
+        double f = 3.9;
+        double quotient = 3/5;
+        System.out.println(quotient);
+        
+        //6)
+        int g = 2;
+        int h = 3;
+        double i = 4.0;
+        double equation = i+g+h;
+        System.out.println(equation);
+        
+        //7)
+        String noun = "Forest";
+        String noun2 = " hills";
+        String town = noun + noun2;
+        System.out.println(town);
+        
+        //8)
+        System.out.println("\\/\\/hat");
+        
+        //9)
+        int j = 4;
+        int k = 9;
+        int l = 2;
+        int result = j*(k+l);
+        System.out.println(j+" *("+ k + " + " + l +") = "+ result);
+    }
+}
